2ebc10889fcbe13e1fa43c923b62430ed5b57a27
techxplorer <corey@servalproject.org>
2012-09-21 17:25:36 +0930

    start adding utility methods for interfacing with sensors

---
+++ src/org/magdaaproject/utils/SensorUtils.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2012 The MaGDAA Project
+ *
+ * This file is part of the MaGDAA Library Software
+ *
+ * MaGDAA Library Software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.magdaaproject.utils;
+
+/**
+ * a utility class which exposes utility methods for interacting with sensors
+ *
+ */
+public class SensorUtils {
+
+	/*
+	 * public class level constants
+	 */
+	
+	/**
+	 * constant to identify the TMP36 temperature sensor
+	 */
+	public static final int TMP36 = 0;
+	
+	/**
+	 * constant to identify the HIH-5031 humidity sensor
+	 */
+	public static final int HIH5031 = 100;
+	
+	
+	/**
+	 * convert a voltage to a temperature
+	 * 
+	 * @param voltage the voltage reading
+	 * @param sensorType the type of sensor used, one of the constants defined by this class
+	 * @param scale the temperature scale to use, one of the constants defined the UnitConversionUtils class
+	 * @return the temperature as derived from the voltage reading 
+	 */
+	public static float convertVoltageToTemp(float voltage, int sensorType, int scale) {
+		
+		float mTemperature;
+		
+		//determine which sensor type is being used
+		switch(sensorType){
+		case TMP36:
+			// this is a TMP36 sensor
+			// by default convert to celsius
+			mTemperature = (voltage - 0.5f) * 100.0f;
+			
+			// convert to the required scale
+			switch(scale) {
+			case UnitConversionUtils.FAHRENHEIT:
+				mTemperature =  UnitConversionUtils.comvertTemperature(mTemperature, UnitConversionUtils.CELSIUS, UnitConversionUtils.FAHRENHEIT);
+			case UnitConversionUtils.KELVIN:
+				mTemperature = UnitConversionUtils.comvertTemperature(mTemperature, UnitConversionUtils.CELSIUS, UnitConversionUtils.KELVIN);
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("the sensorType is invalid");
+		}
+		
+		return mTemperature;
+	}
+	
+	/**
+	 * convert a voltage to a relative humidity
+	 * 
+	 * @param voltage the voltage from the sensor
+	 * @param sensorType the type of sensor used, one of the constants define by this class
+	 * @return the relative humidity derived from the voltage reading
+	 */
+	public static float convertVoltageToRelativeHumidity(float voltage, int sensorType) {
+		
+		float mRelativeHumidity;
+		
+		// determine which sensor type is being used
+		switch(sensorType) {
+		case HIH5031:
+			// this is a HIH-5031 sensor
+			mRelativeHumidity = (1.0f / 0.00636f) * ((voltage / 3.3f) - 0.1515f);
+			break;
+		default:
+			throw new IllegalArgumentException("the sensorType is invalid");
+		}
+		
+		return mRelativeHumidity;
+	}
+	
+	public static float adjustRelativeHumidity(float relativeHumidity, float temperature, int sensorType) {
+		
+		float newHumidity;
+		
+		// determine which sensor is being used
+		switch(sensorType) {
+		case HIH5031:
+			// this is a HIH-5031 sensor
+			newHumidity = relativeHumidity / (1.0546f - (0.00216f * temperature));
+			break;
+		default:
+			throw new IllegalArgumentException("the sensorType is invalid");
+		}
+		
+		return newHumidity;
+	}
+}
---
+++ src/org/magdaaproject/utils/UnitConversionUtils.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2012 The MaGDAA Project
+ *
+ * This file is part of the MaGDAA Library Software
+ *
+ * MaGDAA Library Software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.magdaaproject.utils;
+
+/**
+ * a utility class which exposes utility methods for converting values between different
+ * scales
+ */
+public class UnitConversionUtils {
+	
+	/**
+	 * constant to define the celsius temperature scale
+	 */
+	public static final int CELSIUS = 0;
+	
+	/**
+	 * constant to define the fahrenheit scale
+	 */
+	public static final int FAHRENHEIT = 1;
+	
+	/**
+	 * constant to define the kelvin temperature scale
+	 */
+	public static final int KELVIN = 2;
+	
+	
+	/**
+	 * convert a temperature value from one scale to another
+	 * 
+	 * @param value the temperature
+	 * @param fromScale the scale to convert from
+	 * @param toScale the scale to convert to
+	 * @return the converted temperature
+	 * @throws IllegalArgumentException of the fromScale or toScale parameters are invalid
+	 */
+	public static float comvertTemperature(float value, int fromScale, int toScale) {
+		
+		/*
+		 * determine which conversion to undertake
+		 */
+		switch(fromScale) {
+		case CELSIUS:
+			// convert from celsius
+			switch(toScale) {
+			case FAHRENHEIT:
+				// switch from celsius to fahrenheit
+				return convertFromCelsiusToFahrenheit(value);
+			case KELVIN:
+				// switch from celsius to Kelvin
+				return convertFromCelsiusToKelvin(value);
+			default:
+				throw new IllegalArgumentException("the toScale is invalid");
+			}
+		case FAHRENHEIT:
+			// convert from FAHRENHEIT
+			switch(toScale) {
+			case CELSIUS:
+				// convert from fahrenheit to celsius
+				return convertFromFahrenheitToCelsius(value);
+			case KELVIN:
+				// convert from fahrenheit to kelvin
+				return convertFromFahrenheitToKelvin(value);
+			default:
+				throw new IllegalArgumentException("the toScale is invalid");
+			}
+		case KELVIN:
+			// convert from kelvin
+			switch(toScale) {
+			case CELSIUS:
+				return convertFromKelvinToCelsius(value);
+			case FAHRENHEIT:
+				return convertFromKelvinToFahrenheit(value);
+			default:
+				throw new IllegalArgumentException("the toScale is invalid");
+			}
+		default:
+			throw new IllegalArgumentException("the fromScale was not recognised");
+		}
+	}
+	
+	
+	//TODO implement these methods
+	private static float convertFromCelsiusToFahrenheit(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+	
+	private static float convertFromCelsiusToKelvin(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+	
+	private static float convertFromFahrenheitToCelsius(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+	
+	private static float convertFromFahrenheitToKelvin(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+	
+	private static float convertFromKelvinToCelsius(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+	
+	private static float convertFromKelvinToFahrenheit(float value) {
+		throw new java.lang.UnsupportedOperationException("Method Not Implemented");
+	}
+}
