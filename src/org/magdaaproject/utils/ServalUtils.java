a4890c8e775ba3cb48b9bcdacdf1bb64a3db74a3
techxplorer <corey@servalproject.org>
2012-10-06 22:35:36 +0930

    add functionality to listen for changes in the Serval Mesh software state

--- src/org/magdaaproject/utils/ServalUtils.java
+++ src/org/magdaaproject/utils/ServalUtils.java
@@ -20,7 +20,9 @@
 
 package org.magdaaproject.utils;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 
@@ -33,7 +35,47 @@ public class ServalUtils {
 	 * public class level constants
 	 */
 	
+	/**
+	 * identify the package name of the Serval Mesh software
+	 */
 	public static final String SERVAL_MESH_PACKAGE_NAME = "org.servalproject";
+	
+	/**
+	 * identify the state that the Serval Mesh is on
+	 */
+	public static final int SERVAL_STATUS_ON = 0;
+	
+	/**
+	 * identify the state where the Serval Mesh is off
+	 */
+	public static final int SERVAL_STATUS_OFF = 1;
+	
+	/**
+	 * identify the state whereby the Serval Mesh status is unknown
+	 */
+	public static final int SERVAL_STATUS_UNKOWN = -1;
+	
+	/**
+	 * identify the intent actions used to check Serval State
+	 */
+	public static final String[] SERVAL_STATUS_ACTIONS = {"org.servalproject.ACTION_STATE_CHECK_UPDATE","org.servalproject.ACTION_STATE"};
+	
+	/*
+	 * private class level variables
+	 */
+	/*
+	 * an enum representing the different states of the Serval Mesh software
+	 * derived from the org.serval.project.ServalBatphoneApplication class
+	 */
+	private static enum BatphoneState{
+		Installing,
+		Upgrading,
+		Off,
+		Starting,
+		On,
+		Stopping,
+		Broken
+	}
 	
 	/**
 	 * check to see if the Serval Mesh software is installed
@@ -51,4 +93,61 @@ public class ServalUtils {
 
 		return true;
 	}
+	
+	/**
+	 * get a broadcast receiver that can be used to receive intents about the status
+	 * of the Serval Mesh software
+	 * 
+	 * @return a BroadcastReceiver
+	 */
+	public static BroadcastReceiver getStatusReceiver() {
+		return statusReceiver;
+	}
+	
+	private static BroadcastReceiver statusReceiver = new BroadcastReceiver() {
+		
+		private int status = SERVAL_STATUS_UNKOWN;
+
+		/*
+		 * (non-Javadoc)
+		 * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+		 */
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			
+			// check the intent action
+			if(intent.getAction().equals(SERVAL_STATUS_ACTIONS[0]) || intent.getAction().equals(SERVAL_STATUS_ACTIONS[1])) {
+				
+				// get the state from the intent
+				int mIndicator = intent.getIntExtra("state", -1);
+				
+				// determine which state is indicated
+				if(mIndicator != -1) {
+					BatphoneState mState = BatphoneState.values()[mIndicator];
+					
+					switch(mState) {
+					case On:
+						status = SERVAL_STATUS_ON;
+						break;
+					case Off:
+						status = SERVAL_STATUS_OFF;
+						break;
+					default:
+						status = SERVAL_STATUS_UNKOWN;
+					}
+				} else {
+					status = SERVAL_STATUS_UNKOWN;
+				}
+			}
+		}
+		
+		/**
+		 * get the current status of the Serval Mesh software
+		 * @return
+		 */
+		@SuppressWarnings("unused")
+		public int getStatus() {
+			return status;
+		}
+	};
 }
